28b5f902a81841d81b4f0138b9b05eed3b5ad41e
Fatal1tyPro <Michael_Vostok@mail.ru>
2024-04-02 22:45:02 +0300

    laba9

---
+++ laba9/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        
+            public class Animal
+            {
+                public virtual void Live()
+                {
+                    
+                }
+            }
+
+            public class HerbivoreAnimal : Animal
+            {
+                public override void Live()
+                {
+                    EatGrass();
+                }
+
+                public void EatGrass()
+                {
+                    
+                }
+            }
+
+            public class Predator : Animal
+            {
+                public bool IsHungry { get; set; }
+
+                public override void Live()
+                {
+                    if (IsHungry)
+                    {
+                        Hunt();
+                    }
+                }
+
+                public void Hunt()
+                {
+                    
+                }
+            }
+
+            public class Rabbit : HerbivoreAnimal { }
+
+            public class Deer : HerbivoreAnimal { }
+
+            public class Wolf : Predator { }
+
+            public class Bear : Predator { }
+
+            public class Zoo
+            {
+                public void AcceptAnimal(Animal animal)
+                {
+                    if (animal is HerbivoreAnimal)
+                    {
+                        
+                    }
+                    else if (animal is Predator)
+                    {
+                        
+                    }
+                }
+            }
+
+            public static void Main(string[] args)
+            {
+                
+            }
+        }
+    }
+    
