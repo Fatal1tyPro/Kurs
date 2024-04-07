aa83db56413b54c292573dfdd106e7486026bfb4
Fatal1tyPro <Michael_Vostok@mail.ru>
2024-04-07 13:28:16 +0300

    12 laba

---
+++ laba12.1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+            public interface IAnimal
+        {
+            string Name { get; set; }
+        }
+
+        public interface ICarnivore : IAnimal { }
+
+        public interface IHerbivore : IAnimal { }
+
+        public class Elephant : IHerbivore, IEquatable<Elephant>
+        {
+            public string Name { get; set; }
+            public int Size { get; set; }
+
+            public bool Equals(Elephant other)
+            {
+                if (other == null) return false;
+                return this.Size == other.Size;
+            }
+        }
+
+        public class Zoo
+        {
+            private List<ICarnivore> Carnivores = new List<ICarnivore>();
+            private List<IHerbivore> Herbivores = new List<IHerbivore>();
+
+            public void AddAnimal(IAnimal animal)
+            {
+                switch (animal)
+                {
+                    case ICarnivore carnivore:
+                        Carnivores.Add(carnivore);
+                        break;
+
+                    case IHerbivore herbivore:
+                        Herbivores.Add(herbivore);
+                        break;
+
+                    default:
+                        throw new Exception("Unknown animal type");
+                }
+            }
+        }
+
+    }
+}
+
---
+++ laba12.2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+            public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        public class Person : IComparable<Person>
+        {
+            public int Age { get; set; }
+            public Gender Gender { get; set; }
+
+            public int CompareTo(Person other)
+            {
+                // Девушки перед мужчинами
+                if (Gender != other.Gender)
+                {
+                    return Gender == Gender.Female ? -1 : 1;
+                }
+
+                // Возраст в порядке возрастания
+                return Age.CompareTo(other.Age);
+            }
+        }
+
+        public class PersonComparer : IComparer<Person>
+        {
+            public int Compare(Person x, Person y)
+            {
+                // Девушки перед мужчинами
+                if (x.Gender != y.Gender)
+                {
+                    return x.Gender == Gender.Female ? -1 : 1;
+                }
+
+                // Возраст в порядке возрастания
+                return x.Age.CompareTo(y.Age);
+            }
+        }
+
+        public class MobileNetwork
+        {
+            // ...
+        }
+
+        public class MobilePhone<T> where T : MobileNetwork
+        {
+            public T Network { get; set; }
+        }
+
+        public void Run()
+        {
+            List<Person> listOfPerson = new List<Person>();
+            // Добавьте 10 человек, 5 девушек, 5 мужчин
+            // ...
+
+            // Сортировка с помощью IComparable
+            listOfPerson.Sort();
+
+            // Сортировка с помощью IComparer
+            listOfPerson.Sort(new PersonComparer());
+
+            MobilePhone<MobileNetwork> phone = new MobilePhone<MobileNetwork>();
+            // ...
+        }
+
+    }
+}
+
